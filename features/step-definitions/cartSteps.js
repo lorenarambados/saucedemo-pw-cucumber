b0b6558f87f51a61d8c3c97199a7727aff58dfd7
lorenarambados <lorenarambados@gmail.com>
2025-06-28 15:26:56 -0300

    feat: enable hooks with correct project structure

--- features/step-definitions/cartSteps.js
+++ features/step-definitions/cartSteps.js
@@ -1,19 +1,10 @@
-import { Given, When, Then, After } from '@cucumber/cucumber';
-import { chromium } from 'playwright';
+import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
-import LoginPage from '../../pages/loginPage.js';
-import InventoryPage from '../../pages/inventoryPage.js';
+import { loginUser } from '../../helpers/loginHelper.js';
 
 Given('I am logged in', async function () {
-  this.browser = await chromium.launch({ headless: true });
-  this.context = await this.browser.newContext();
-  this.page = await this.context.newPage();
-
-  const loginPage = new LoginPage(this.page);
-  await loginPage.navigate();
-  await loginPage.login('standard_user', 'secret_sauce');
-
-  this.inventoryPage = new InventoryPage(this.page);
+  const { inventoryPage } = await loginUser(this.page);
+  this.inventoryPage = inventoryPage;
 });
 
 When('I add the first product to the cart', async function () {
@@ -37,10 +28,4 @@ When('I remove the first product from the cart', async function () {
 Then('the cart badge should not be visible', async function () {
   const badge = this.page.locator('.shopping_cart_badge');
   await expect(badge).toHaveCount(0);
-});
-
-After(async function () {
-  if (this.page) await this.page.close();
-  if (this.context) await this.context.close();
-  if (this.browser) await this.browser.close();
 });
